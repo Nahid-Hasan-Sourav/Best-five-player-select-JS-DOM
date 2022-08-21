5a6f402f2fd7b961fe1056a9e282a2535077e0f2
Nahid-Hasan_Sourav <nahidhasansourav05@gmail.com>
2022-08-22 04:12:35 +0600

    already finished calculate btn task and validation for all input field

---
+++ Js/app.js
@@ -0,0 +1,72 @@
+// common function for select all input field value convert it in number type and return it.
+function selectInputFieldValue(elementId){
+    const inputField=document.getElementById(elementId);
+    const inputFieldString=inputField.value;
+    console.log("InputFieldString: " + inputFieldString);
+    const inputFieldNumber=Number(inputFieldString);
+    
+    console.log("InputFieldNumber ",inputFieldNumber);
+
+    if( inputFieldString.length===0){
+        alert("No Field Can not be empty");
+        return 0;
+    }
+    else if( inputFieldNumber<0){
+        alert("Number must be greater than 0");
+        return 0;
+    }
+    else if(isNaN(inputFieldNumber)==true){
+        alert("Input must be a number");
+        return  0;
+    }
+
+    else{
+        return inputFieldNumber;
+
+    }
+   
+}
+
+
+function displaySetValue(elementId,value){
+    const inputField=document.getElementById(elementId);
+    inputField.innerText=value;
+    
+}
+
+const allBtn=document.querySelectorAll(".btn-common");
+for(const btn of allBtn){
+
+    btn.addEventListener("click", function(){
+        const All=btn.parentNode.children[0]
+        const playerName=All.innerText;
+        const selectTag=document.getElementById("ol")
+        const li=document.createElement("li");
+       
+
+        const length=selectTag.children.length;
+        // console.log(length);
+
+        if(length>=5){
+            alert("sorry!! you can select only 5 players")
+        }
+        else{
+            li.innerText=playerName;
+            selectTag.appendChild(li);
+            btn.disabled = true;
+        }
+       
+        
+        
+    });
+}
+
+document.getElementById("calculate-expense").addEventListener("click", function(){
+    const selectTag=document.getElementById("ol");
+    const length=selectTag.children.length;
+    console.log("calculate-btn",length);
+
+    const perPlayerCoast=selectInputFieldValue("per-players");
+    console.log("perPlayerCoast : ",perPlayerCoast);
+
+});
